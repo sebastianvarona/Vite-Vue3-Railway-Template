6c1cfcc96b3e78f391859ce84ac8032d1db45b04
sebastianvarona <sebastianvarona@gmail.com>
2022-11-11 09:55:07 -0500

    initial configuration

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="h-screen flex justify-center items-center">
+    <h1 class="text-9xl font-bold">Barbersh.app 💈</h1>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
